9f82877388784ef09c39c4ded1664bf78906e7a7
keksroom <keksroom@gmail.com>
2022-12-20 12:26:08 +0300

    Added logic for TrafficLight

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redTrafficLightView: UIView!
+    @IBOutlet weak var yellowTrafficLightView: UIView!
+    @IBOutlet weak var greenTrafficLightView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 15
+        redTrafficLightView.layer.cornerRadius = 60
+        redTrafficLightView.backgroundColor = #colorLiteral(red: 0.5527142397, green: 0.1346355199, blue: 0.1346355199, alpha: 1)
+        yellowTrafficLightView.layer.cornerRadius = 60
+        yellowTrafficLightView.backgroundColor = #colorLiteral(red: 0.5435929446, green: 0.5262179572, blue: 0.2287210027, alpha: 1)
+        greenTrafficLightView.layer.cornerRadius = 60
+        greenTrafficLightView.backgroundColor = #colorLiteral(red: 0.09598759765, green: 0.5079333119, blue: 0.2253593592, alpha: 1)
     }
 
-
+    @IBAction func startButtonTapped() {
+        if redTrafficLightView.backgroundColor == #colorLiteral(red: 0.5527142397, green: 0.1346355199, blue: 0.1346355199, alpha: 1) && yellowTrafficLightView.backgroundColor == #colorLiteral(red: 0.5435929446, green: 0.5262179572, blue: 0.2287210027, alpha: 1) && greenTrafficLightView.backgroundColor == #colorLiteral(red: 0.09598759765, green: 0.5079333119, blue: 0.2253593592, alpha: 1) {
+            redTrafficLightView.backgroundColor = #colorLiteral(red: 1, green: 0.2435897436, blue: 0.2435897436, alpha: 1)
+            startButton.setTitle("Next", for: .normal)
+        } else if redTrafficLightView.backgroundColor == #colorLiteral(red: 1, green: 0.2435897436, blue: 0.2435897436, alpha: 1) && yellowTrafficLightView.backgroundColor == #colorLiteral(red: 0.5435929446, green: 0.5262179572, blue: 0.2287210027, alpha: 1) && greenTrafficLightView.backgroundColor == #colorLiteral(red: 0.09598759765, green: 0.5079333119, blue: 0.2253593592, alpha: 1) {
+            redTrafficLightView.backgroundColor = #colorLiteral(red: 0.5527142397, green: 0.1346355199, blue: 0.1346355199, alpha: 1)
+            yellowTrafficLightView.backgroundColor = #colorLiteral(red: 0.9706829897, green: 0.9391655999, blue: 0.4046222146, alpha: 1)
+        } else if redTrafficLightView.backgroundColor == #colorLiteral(red: 0.5527142397, green: 0.1346355199, blue: 0.1346355199, alpha: 1) && yellowTrafficLightView.backgroundColor == #colorLiteral(red: 0.9706829897, green: 0.9391655999, blue: 0.4046222146, alpha: 1) && greenTrafficLightView.backgroundColor == #colorLiteral(red: 0.09598759765, green: 0.5079333119, blue: 0.2253593592, alpha: 1) {
+            yellowTrafficLightView.backgroundColor = #colorLiteral(red: 0.5435929446, green: 0.5262179572, blue: 0.2287210027, alpha: 1)
+            greenTrafficLightView.backgroundColor = #colorLiteral(red: 0.1722537255, green: 0.9449097939, blue: 0.414533877, alpha: 1)
+        } else if redTrafficLightView.backgroundColor == #colorLiteral(red: 0.5527142397, green: 0.1346355199, blue: 0.1346355199, alpha: 1) && yellowTrafficLightView.backgroundColor == #colorLiteral(red: 0.5435929446, green: 0.5262179572, blue: 0.2287210027, alpha: 1) && greenTrafficLightView.backgroundColor == #colorLiteral(red: 0.1722537255, green: 0.9449097939, blue: 0.414533877, alpha: 1) {
+            redTrafficLightView.backgroundColor = #colorLiteral(red: 1, green: 0.2435897436, blue: 0.2435897436, alpha: 1)
+            greenTrafficLightView.backgroundColor = #colorLiteral(red: 0.09598759765, green: 0.5079333119, blue: 0.2253593592, alpha: 1)
+        }
+    }
 }
+        
+    
 
